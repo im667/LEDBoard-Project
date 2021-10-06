92be43474fb119badbadf9f914f784244d18beb5
kwon <koh9037@gmail.com>
2021-10-06 16:40:45 +0900

    finish

--- LEDBoard-Project/LEDViewController.swift
+++ LEDBoard-Project/LEDViewController.swift
@@ -20,20 +20,34 @@ func getRandomColor() -> UIColor{
     }
 
 
-class LEDViewController: UIViewController {
 
+
+class LEDViewController: UIViewController, UITextFieldDelegate, UIGestureRecognizerDelegate {
+    
+    
     @IBOutlet var InputTf: UITextField!
     
     @IBOutlet var outputLabel: UILabel!
     
+    @IBOutlet var ChangeColorButton: UIButton!
+    
+    @IBOutlet var SendButton: UIButton!
+    
+    @IBOutlet var InputWrap: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    
+        InputTf.delegate = self
+        outputLabel.adjustsFontSizeToFitWidth = true
     }
     
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.InputTf.endEditing(true)
+    }
+    
     @IBAction func SendBtn(_ sender: UIButton) {
         outputLabel.text = InputTf.text
     }
@@ -41,6 +55,25 @@ class LEDViewController: UIViewController {
     
     @IBAction func ChangeColorBtn(_ sender: UIButton) {
         self.outputLabel.textColor = getRandomColor()
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       
+        self.InputTf.resignFirstResponder()
+        SendBtn(SendButton)
+        return true
+    }
+    
+    
+    @IBAction func isClickedTap(_ sender: UITapGestureRecognizer) {
+        if InputWrap.isHidden {
+            InputWrap.isHidden = false
+            print("tap")
+        } else {
+            InputWrap.isHidden = true
+        }
     }
     
 }
